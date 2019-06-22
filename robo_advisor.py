a98fa2d6999b85e9c4258f98b48c506a08d2fed6
kris10s7 <50898383+kris10s7@users.noreply.github.com>
2019-06-22 18:14:33 -0400

    Get latest closing price

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+# source: https://github.com/prof-rossetti/nyu-info-2335-201905/edit/master/notes/python/datatypes/numbers.md
+def to_usd(my_price):
+    return f"${my_price:,.2f}" #> $12,000.71
 
 #
 # INFO INPUTS
@@ -10,13 +13,12 @@ import json
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
-#print(type(response))
-#print(response.status_code)
-#print(response.text)
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"] #> 1,000.00
 
 #breakpoint()
 
@@ -32,7 +34,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
