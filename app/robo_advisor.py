ce542d84a97944058a31d1b191ce62cbc5080ab7
kris10s7 <50898383+kris10s7@users.noreply.github.com>
2019-06-23 17:14:00 -0400

    Write all dialy prices to CSV

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -42,8 +42,8 @@ low_prices = []
 
 for date in dates:
     high_price = tsd[date]["2. high"]
-    high_prices.append(high_price)
     low_price = tsd[date]["3. low"]
+    high_prices.append(high_price)
     low_prices.append(low_price)
 
 recent_high = max(high_prices)
@@ -64,24 +64,17 @@ csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
     writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader() # uses fieldnames set above
-
-    # Looping
-    writer.writerow({
-        "timestamp": "TODO",
-        "open": "TODO",
-        "high": "TODO",
-        "low": "TODO",
-        "close": "TODO",
-        "volume": "TODO",
-    })
-    writer.writerow({
-        "timestamp": "TODO",
-        "open": "TODO",
-        "high": "TODO",
-        "low": "TODO",
-        "close": "TODO",
-        "volume": "TODO",
-    })
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+        })
+ 
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
