9affde99f66f31cb6eb3055db4b5949c93cfe874
kris10s7 <50898383+kris10s7@users.noreply.github.com>
2019-06-30 01:49:20 -0400

    sector performance chart

--- app/sector_performance.py
+++ app/sector_performance.py
@@ -33,26 +33,61 @@ response = requests.get(request_url)
 parsed_response = json.loads(response.text)  # parse json into a dictionary
 last_refreshed = parsed_response["Meta Data"]["Last Refreshed"]
 
-##input sector
+##retrieving sector data for sector input by user
+
 input_sector = input("Please input sector: ")
 
-one_day_performance = parsed_response["Rank B: 1 Day Performance"][input_sector]
-one_month_performance = parsed_response["Rank D: 1 Month Performance"][input_sector]
-three_month_performance = parsed_response["Rank E: 3 Month Performance"][input_sector]
-YTD_performance = parsed_response["Rank F: Year-to-Date (YTD) Performance"][input_sector]
+titlecase_input_sector = input_sector.title() #convert user input to title case https://www.geeksforgeeks.org/title-in-python/
 
-trace = go.Table(
-    header=dict(values=[' ', '1Day Performance', '1M Performance', '3 Month Performance', 'YTD performance'],
-                line = dict(color='#ffffff'),
-                fill = dict(color='#ddd3ee'),
-                align = ['left'] * 5),
-    cells=dict(values=[[input_sector], [one_day_performance],[one_month_performance], [three_month_performance], [YTD_performance]],
-               line = dict(color='#ddd3ee'),
-               fill = dict(color='#ffffff'),
-               align = ['left'] * 5))
+one_day_performance = parsed_response["Rank B: 1 Day Performance"][titlecase_input_sector]
+one_month_performance = parsed_response["Rank D: 1 Month Performance"][titlecase_input_sector]
+three_month_performance = parsed_response["Rank E: 3 Month Performance"][titlecase_input_sector]
+YTD_performance = parsed_response["Rank F: Year-to-Date (YTD) Performance"][titlecase_input_sector]
 
-layout = dict(width=500, height=300)
-data = [trace]
-fig = dict(data=data, layout=layout)
-py.iplot(fig, filename = 'styled_table')
+##Retrieving sector data
 
+one_day = parsed_response["Rank B: 1 Day Performance"]
+one_month = parsed_response["Rank D: 1 Month Performance"]
+three_month = parsed_response["Rank E: 3 Month Performance"]
+YTD = parsed_response["Rank F: Year-to-Date (YTD) Performance"]
+
+a = list(one_day[titlecase_input_sector])
+a.remove('%')
+one_day_converted = float("".join(a))
+
+b = list(one_month[titlecase_input_sector])
+b.remove('%')
+one_month_converted = float("".join(b))
+
+c = list(three_month[titlecase_input_sector])
+c.remove('%')
+three_month_converted = float("".join(c))
+
+d = list(YTD[titlecase_input_sector])
+d.remove('%')
+YTD_converted = float("".join(d))
+
+#https://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
+
+#Creating the sector performance chart
+
+x = ['1Day Perf. (%)', '1M Perf. (%)', '3M Perf. (%)', 'YTD Perf. (%)']
+y = [one_day_converted, one_month_converted, three_month_converted, YTD_converted]
+
+data = [go.Bar(
+            x=x,
+            y=y,
+            text=y,
+            textposition = 'auto',
+            marker=dict(
+                color='rgb(106,90,205)',
+                line=dict(
+                    color='rgb(8,48,107)',
+                    width=1.5),
+            ),
+            opacity=0.6
+        )]
+
+py.iplot(data, filename='bar-direct-labels')
+
+print('Sector Performance chart has been successfully created.')
